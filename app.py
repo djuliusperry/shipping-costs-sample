6a66cb9e7d098f819434988960b39eef55df8994
djuliusperry <djuliusperry@gmail.com>
2017-03-12 15:43:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,10 @@
 import urllib
 import json
 import os
-import beatbox
-
 
 from flask import Flask
 from flask import request
 from flask import make_response
-
 
 # Flask app should start in global layout
 app = Flask(__name__)
@@ -36,8 +33,8 @@ def makeWebhookResult(req):
     result = req.get("result")
     parameters = result.get("parameters")
     zone = parameters.get("shipping-zone")
-    
-    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':5000, 'Africa':500}
+
+    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
     speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
 
@@ -54,9 +51,66 @@ def makeWebhookResult(req):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
-    #port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 5000))
 
-    #print "Starting app on port %d" % port
+    print "Starting app on port %d" % port
 
-    #app.run(debug=True, port=port, host='0.0.0.0')
+    app.run(debug=True, port=port, host='0.0.0.0')
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+    if req.get("result").get("action") != "shipping.cost":
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    zone = parameters.get("shipping-zone")
+
+    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+
+    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        #"data": {},
+        # "contextOut": [],
+        "source": "apiai-onlinestore-shipping"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=True, port=port, host='0.0.0.0')
