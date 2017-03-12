f641fd35e202fcf46831cea13d8edfc66fe11ebf
djuliusperry <djuliusperry@gmail.com>
2017-03-12 19:46:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -101,10 +101,11 @@ def makeWebhookResult(req):
     #cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':800, 'Africa':500}
     amount = {'1234567':4387, '7654321':3350}
     name = {'1234567':'Perry Dominguez', '7654321':'Carlos Garcia'}
+    due = {'1234567':'March 5, 2017', '7654321':'March 6, 2017'}
 
     #speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
     #speech = "The cost of shipping to " + zone + " is " + "159 " + " euross."
-    speech = "Thank you for that information Mr. " + str(name[sin]) + ". Your bill amount for the month of " + billmonth + " is "  + str(amount[sin]) + " pesos. Do you want me to do an analysis on your bill?"
+    speech = "Thank you for that information Mr. " + str(name[sin]) + ". Your bill amount for the month of " + billmonth + " is "  + str(amount[sin]) + " pesos. This is due on " + str(due[sin]) +". Do you want me to do an analysis on your bill?"
     #speech = "Thank you for that information Mr. The bill amount for the month of " + billmonth + "for SIN " + sin + " is "  + str(amount[sin]) + " pesos. Do you want me to do analysis on your bill?"
                            
     print("Response:")
