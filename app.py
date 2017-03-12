010f178ceb682c906076d5eaaed93e31d5288a4f
djuliusperry <djuliusperry@gmail.com>
2017-03-12 16:22:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -96,9 +96,9 @@ def makeWebhookResult(req):
     
     #con = p.connect("dbname"='dcf8dpgo20qc84' user='udbunywtjpldqr' host ='ec2-23-21-238-246.compute-1.amazonaws.com'")
     con = psycopg2.connect("dbname"='dcf8dpgo20qc84' user='udbunywtjpldqr' host ='ec2-23-21-238-246.compute-1.amazonaws.com'")
-                    cur = con.cursor()
-                    cur.execute(select cost from shipping_zones where id=3)
-                    rows = cur.fetachall()
+                    #cur = con.cursor()
+                    #cur.execute(select cost from shipping_zones where id=3)
+                    #rows = cur.fetachall()
                     
                     #for row in rows:
                     #cost = row[0]
