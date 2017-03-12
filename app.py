80d08aa3cb937721567ac6da45ed472b92a90524
djuliusperry <djuliusperry@gmail.com>
2017-03-12 14:47:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,10 +38,10 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     zone = parameters.get("shipping-zone")
 
-    con = p.connect("dbname"='dcf8dpgo20qc84' user='udbunywtjpldqr' host ='ec2-23-21-238-246.compute-1.amazonaws.com'")
-                    cur = con.cursor()
-                    cur.execute(select cost from shipping_zones where id=3)
-                    rows.cur.fetachall()
+    #con = p.connect("dbname"='dcf8dpgo20qc84' user='udbunywtjpldqr' host ='ec2-23-21-238-246.compute-1.amazonaws.com'")
+                    #cur = con.cursor()
+                    #cur.execute(select cost from shipping_zones where id=3)
+                    #rows.cur.fetachall()
                     
                     #for r in rows:
                     #cost = r["cost"]
