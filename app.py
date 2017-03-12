b9ada21e50edb15b7aa59caa53bb36a4e798a349
djuliusperry <djuliusperry@gmail.com>
2017-03-12 14:22:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ import urllib
 import json
 import os
 import beatbox
+import psycopg2 as p
 
 
 from flask import Flask
 from flask import request
 from flask import make_response
+
 
 # Flask app should start in global layout
 app = Flask(__name__)
@@ -36,7 +38,15 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     zone = parameters.get("shipping-zone")
 
-    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':5000, 'Africa':500}
+    con = p.connect("dbname"='dcf8dpgo20qc84' user='udbunywtjpldqr' host ='ec2-23-21-238-246.compute-1.amazonaws.com'")
+                    cur = con.cursor()
+                    cur.execute(select cost from shipping_zones where id=3)
+                    rows.cur.fetachall()
+                    
+                    for r in rows:
+                    cost = r["cost"]
+    
+    #cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':5000, 'Africa':500}
 
     speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
 
